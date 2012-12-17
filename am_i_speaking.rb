670fb3da182bfbcf21cad587a97f2a973d024ed7
Chris Hunt <c@squareup.com>
2012-12-17 10:46:22 -0800

    Am I speaking? Probably not

--- am_i_speaking.rb
+++ am_i_speaking.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 class AmISpeaking
-  attr_reader :my_talk
+  attr_reader :my_talk, :my_talk_ranking
 
   class Color
     require 'term/ansicolor'
@@ -22,9 +22,14 @@ class AmISpeaking
   def run
     print_proposals
     print_summary
+    print_result
   end
 
   private
+
+  def speaker_count
+    10
+  end
 
   def html
     @html ||= Nokogiri::HTML(open('http://larubyconf.com/proposals'))
@@ -51,14 +56,27 @@ class AmISpeaking
   end
 
   def print_summary
-    print Color.green, "#{proposals.size} proposals, #{total_votes} votes.\n"
+    print Color.green, "#{proposals.size} proposals, #{total_votes} votes.\n\n"
     print Color.clear
+  end
+
+  def print_result
+    print 'Am I speaking? '
+
+    if my_talk_ranking.nil?
+      puts 'Who knows. I could not find it.'
+    elsif my_talk_ranking <= speaker_count
+      puts 'Probably.'
+    else
+      puts 'Probably not.'
+    end
   end
 
   def print_proposals
     proposals.sort.each_with_index do |proposal, index|
+      proposal_rank = (proposals.size - index)
       print Color.blue
-      print "%02d" % (proposals.size - index)
+      print "%02d" % proposal_rank
       print " "
 
       print Color.red, "("
@@ -67,6 +85,7 @@ class AmISpeaking
 
       if proposal.title.downcase == my_talk
         print Color.yellow, Color.bold
+        @my_talk_ranking = proposal_rank
       end
 
       print proposal.title, Color.clear, "\n"
